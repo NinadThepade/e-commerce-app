caca7bf8b25dd28dad41d6b6302cae2c63bcf727
ninadthepade <ninad.thepade@gmail.com>
2020-05-21 19:40:54 +0530

    Updates

---
+++ precache-manifest.d6b2ace558fe16d2834e088b5c037bf1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fac0f3ec0feaf5458f02e76b5792630",
+    "url": "/e-commerce-app/index.html"
+  },
+  {
+    "revision": "4e98f2f90c281fc76303",
+    "url": "/e-commerce-app/static/css/main.7ef98ddc.chunk.css"
+  },
+  {
+    "revision": "58915ff67db5fee6cd5a",
+    "url": "/e-commerce-app/static/js/2.e5abd343.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/e-commerce-app/static/js/2.e5abd343.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e98f2f90c281fc76303",
+    "url": "/e-commerce-app/static/js/main.8ec2328a.chunk.js"
+  },
+  {
+    "revision": "f175544e30bd50c588cc",
+    "url": "/e-commerce-app/static/js/runtime-main.31008937.js"
+  }
+]);
